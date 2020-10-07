f5e186442d6baedfbe3703418ef0ab13c4cbc0d5
zotacy <tacyzacharyo@johndeere.com>
2020-10-07 10:12:07 -0400

    add js to click cells

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('connected')
+//console.log('connected')
+
+const grid = document.querySelector('.grid')
+const cellArray = document.querySelectorAll('.cell')
+
+function cellClick(){
+    console.log(`You clicked a cell!`);
+};
+cellArray.forEach((cell) => cell.addEventListener('click', cellClick))
