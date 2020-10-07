7a7dde66717521de707fce821f44468f5954d95f
zotacy <tacyzacharyo@johndeere.com>
2020-10-07 12:05:06 -0400

    part 1 complete. Grid setup, chnage cells based on player turn. prevent changing a clicked cell. reset button

--- script.js
+++ script.js
@@ -2,8 +2,29 @@
 
 const grid = document.querySelector('.grid')
 const cellArray = document.querySelectorAll('.cell')
+const resetButton = document.querySelector("#reset")
 
-function cellClick(){
-    console.log(`You clicked a cell!`);
+let turn ='x'
+function cellClick(event){
+    if (turn == 'x' && this.classList.contains('clicked') == false){
+        this.classList.add('clicked')
+        this.style.backgroundColor = 'blue';
+        turn = 'o'
+    } else if (turn == 'o' && this.classList.contains('clicked') == false){
+        this.style.backgroundColor = 'red';
+        this.classList.add('clicked')
+        turn ='x'
+    };
 };
-cellArray.forEach((cell) => cell.addEventListener('click', cellClick))
+cellArray.forEach(cell => {
+    cell.addEventListener('click', cellClick)
+});
+resetButton.addEventListener('click',()=>{
+    cellArray.forEach(cell => {
+        if (cell.classList.contains('clicked') == true){
+            cell.classList.remove('clicked')
+            cell.style.backgroundColor = 'white'
+        }
+    });
+});
+
